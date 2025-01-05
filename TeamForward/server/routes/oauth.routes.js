897d1a4e092e4e5c3e5c714dd6b782b200cc159e
Sarah Murphree <104289709+smurph7894@users.noreply.github.com>
2025-01-05 15:04:11 -0800

    adding comments and commenting out unused code

--- TeamForward/server/routes/oauth.routes.js
+++ TeamForward/server/routes/oauth.routes.js
@@ -2,24 +2,27 @@ const router = require("express").Router();
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 
-router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+// //TODO: do i need this without passport login used?
+// router.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["profile", "email"] })
+// );
 
-router.get(
-  "/oauth2callback",
-  passport.authenticate("google", {
-    successRedirect: process.env.REDIRECTKEYONE,
-    failureRedirect: "/signin/failed",
-    successMessage: true,
-    failureMessage: true,
-  })
-);
+// //TODO: do i need this without passport login used?
+// router.get(
+//   "/oauth2callback",
+//   passport.authenticate("google", {
+//     successRedirect: process.env.REDIRECTKEYONE,
+//     failureRedirect: "/signin/failed",
+//     successMessage: true,
+//     failureMessage: true,
+//   })
+// );
 
 router.get("/signin/success", (req, res) => {
   if (req.user) {
     const payload = { id: req.user._id };
+    // sign transforms payload into a token
     const userToken = jwt.sign(payload, process.env.SecretKeyOne);
     res.cookie("jwt-token", userToken, { httpOnly: true });
     res.status(200).json({
